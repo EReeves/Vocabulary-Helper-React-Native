f5bba739624d3f182c7bff292206d362b025a35e
ereeves <evanreeves22@gmail.com>
2018-04-21 04:34:13 +1000

    remove built javascript files from repo

--- jsbin/util/palette.js
+++
@@ -1,9 +0,0 @@
-const Palette = {
-    BackgroundDark: "#2B271C",
-    White: "#FEE6BF",
-    ActiveRed: "#E82C0C",
-    BackgroundLight: "#43686A",
-    Highlight: "#8E756F",
-};
-export default Palette;
-//# sourceMappingURL=palette.js.map
